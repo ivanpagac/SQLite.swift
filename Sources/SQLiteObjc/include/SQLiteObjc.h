8d859f43c3a3da586a21bd6b3d183311bcb11dcf
ivanpagac <ivanpagac@gmail.com>
2021-04-06 20:45:18 +0200

    Update SQLiteObjc.h

--- Sources/SQLiteObjc/include/SQLiteObjc.h
+++ Sources/SQLiteObjc/include/SQLiteObjc.h
@@ -22,9 +22,13 @@
 // THE SOFTWARE.
 //
 
-@import Foundation;
+@@import Foundation;
 #if defined(SQLITE_SWIFT_STANDALONE)
 @import sqlite3;
+
+#elif defined(SQLITE_SWIFT_SQLCIPHER)
+@import SQLCipher;
+
 #else
 @import SQLite3;
 #endif
